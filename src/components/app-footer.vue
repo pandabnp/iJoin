e6ea672f123a24d12c5a54bc3bb9a4011169c7ba
Baidyanath Panda <baidyanath@icloud.com>
2018-10-28 21:44:04 -0400

    test

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
   v-footer.my-appfooter(:class="{ 'my-appfooter--sidebar-open': sidebarVisibility }")
-    .grey--text © 2018 Travelers 
+    .grey--text © 2018 Your Company Here
 </template>
 
 <script>
 export default {
   name: 'AppFooter',
-
   computed: {
     sidebarVisibility () {
       return this.$store.state.common.sidebar.visible && this.$vuetify.breakpoint.mdAndUp
@@ -17,10 +16,7 @@ export default {
 
 <style lang="stylus">
 .my-appfooter
-  justify-content: center,
-   background-color: red
-
+  justify-content: center
   &--sidebar-open
     padding-left: 300px
-
 </style>
