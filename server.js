fe06e09bf60713929c426ed9d0e862322cbca9c5
Baidyanath Panda <baidyanath@icloud.com>
2018-10-28 23:38:26 -0400

    tesy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var serveStatic = require('serve-static');
 app = express();
 console.log('route '+ __dirname + "/dist");
 app.use(serveStatic(__dirname + "/dist"));
+//app.use(express.static(path.join(__dirname+ "/dist", "js")));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
